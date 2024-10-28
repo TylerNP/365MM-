c50b234545f41056db335bd6096b9aa233947b60
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2024-10-27 20:20:04 -0700

    implemented movie interested

--- src/api/movies.py
+++ src/api/movies.py
@@ -30,15 +30,8 @@ def get_movie(movie_id : int):
     with db.engine.begin() as connection:
         sql_to_execute = "SELECT name, release_year, genres, average_rating, budget, box_office, demographic FROM movies WHERE id = :movie_id"
         result = connection.execute(sqlalchemy.text(sql_to_execute), {"movie_id":movie_id})
-    for info in result:
-        movie["movie_id"] = movie_id
-        movie["name"] = info.name
-        movie["release_year"] = info.release_year
-        movie["genres"] = info.genres
-        movie["average_rating"] = info.average_rating
-        movie["budget"] = info.budget
-        movie["box_office"] = info.box_office
-        movie["demographic"] = info.demographic
+        movie = format_movie(result, movie_id)
+    print(movie)
     return movie
 
 @router.post("/new/")
@@ -46,6 +39,7 @@ def new_movie(new_movie : Movie):
     """
     Create a new entry for a movie
     """
+    print(new_movie)
     movie_id = 0
     with db.engine.begin() as connection:
         sql_to_execute = """
@@ -75,5 +69,52 @@ def get_movie_available():
     return NotImplemented
 
 @router.get("/user/{user_id}")
-def get_movie_interested():
-    return NotImplemented
+def get_movie_interested(user_id : int):
+    """
+    Gets a random movie that a user has not watched, empty if no movies available
+    """
+    movie = {}
+    with db.engine.begin() as connection:
+        sql_to_execute = """
+                            SELECT 
+                                movies.id, 
+                                movies.name, 
+                                movies.release_year,
+                                movies.genres,
+                                movies.average_rating,
+                                movies.budget,
+                                movies.box_office, 
+                                movies.demographic 
+                            FROM 
+                                movies
+                            WHERE NOT EXISTS (
+                                SELECT 1 
+                                FROM watched_movies 
+                                WHERE user_id = :user_id
+                                AND movie_id = movies.id
+                            ) 
+                            ORDER BY 
+                                RANDOM() 
+                            LIMIT 1
+                        """
+        results = list(connection.execute(sqlalchemy.text(sql_to_execute), {"user_id":user_id}))
+        movie_id = 0
+        for result in results:
+            movie_id = result.id 
+        movie = format_movie(results, movie_id)
+        print(movie)
+    return movie
+
+# Takes CursorResult Object And Converts into Movie Dictionary For Json
+def format_movie(movie_result : object, movie_id : int) -> dict[str, any]:
+    movie = {}
+    for info in movie_result:
+        movie["movie_id"] = movie_id
+        movie["name"] = info.name
+        movie["release_year"] = info.release_year
+        movie["genres"] = info.genres
+        movie["average_rating"] = info.average_rating
+        movie["budget"] = info.budget
+        movie["box_office"] = info.box_office
+        movie["demographic"] = info.demographic
+    return movie
