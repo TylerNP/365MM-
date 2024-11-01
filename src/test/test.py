a1516467fb570ce1be0581e43b0499317c14b807
Tyler Pham <137601404+TylerNP@users.noreply.github.com>
2024-11-01 10:47:23 -0700

    setup unittest

---
+++ src/test/test.py
@@ -0,0 +1,9 @@
+import unittest
+from src.api import users
+
+class Test_users(unittest.TestCase):
+    def test_1(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
